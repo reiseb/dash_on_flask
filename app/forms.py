8818e71de102e8d501d1301d788c42e02f503307
reiseb <48870100+reiseb@users.noreply.github.com>
2020-03-15 01:36:06 +0100

    add length validation to forms

--- app/forms.py
+++ app/forms.py
@@ -3,17 +3,48 @@ from wtforms import BooleanField
 from wtforms import PasswordField
 from wtforms import StringField
 from wtforms import SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(
+        'Username',
+        render_kw={
+            'placeholder': 'username',
+            'required': 'True',
+            'maxlength': 10
+        },
+        validators=[DataRequired(), Length(max=10)]
+    )
+    password = PasswordField(
+        'Password',
+        render_kw={
+            'placeholder': 'password',
+            'required': 'True',
+            'maxlength': 25
+        },
+        validators=[DataRequired(), Length(max=25)]
+    )
     submit = SubmitField('Login')
     remember_me = BooleanField('Remember Me')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(
+        'Username',
+        render_kw={
+            'placeholder': 'username',
+            'required': 'True',
+            'maxlength': 10
+        },
+        validators=[DataRequired(), Length(max=10)]
+    )
+    password = PasswordField(
+        'Password',
+        render_kw={
+            'placeholder': 'password',
+            'required': 'True',
+            'maxlength': 25
+        },
+        validators=[DataRequired(), Length(max=25)])
     submit = SubmitField('Register')
